6619e356df8c3b714b6b3f7d608934f98068e7c7
Tuan Pham <patuan08092003@gmail.com>
2025-11-17 16:40:41 +0700

    first commit

---
+++ src/test/java/com/horacia/server/HoraciaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horacia.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraciaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
